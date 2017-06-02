f6b5e366aba2c0695f6b25effac62ffaa9d9fb6f
xaljox <jimmy.venema@gmail.com>
2017-06-02 08:00:30 +0200

    work in progress

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ class RegEx : public regex_t
 
 class Option : public option
 {
-    static const char* descr[64];
+    static string descr[64];
     static const char* argname[64];
     static int cnt;
 
@@ -36,18 +36,28 @@ class Option : public option
         has_arg = ha;
         flag = f;
 	    val = v;
-        descr[cnt] = d;
-        argname[cnt++] = an;
+        argname[cnt] = an;
+        string tmp = d;
+        while (tmp.length() > 41)
+        {
+            int pos = tmp.rfind(' ', 41);
+            if (pos == string::npos)
+                break;
+            descr[cnt] += tmp.substr(0, pos);
+            descr[cnt] += '\n';
+            descr[cnt].append(39, ' ');
+            tmp.erase(0, pos+1);
+        }
+        descr[cnt++] += tmp;
     }
-
-    void PrintUsage(const char* d, const char* an) const
+    void PrintUsage(const string& d, const char* an) const
     {
         if (has_arg == 0)
-            fprintf(stdout, "  --%-15s (-%c)               %s\n", name, val, d);
+            fprintf(stdout, "  --%-15s (-%c)               %s\n", name, val, d.c_str());
         else if (has_arg == 1)
-            fprintf(stdout, "  --%-15s (-%c) %-12s  %s\n", name, val, an, d);
+            fprintf(stdout, "  --%-15s (-%c) %-12s  %s\n", name, val, an, d.c_str());
         else if (has_arg == 2)
-            fprintf(stdout, "  --%-15s (-%c) [%-10s] %s\n", name, val, an, d);
+            fprintf(stdout, "  --%-15s (-%c) [%-10s] %s\n", name, val, an, d.c_str());
     }
     void Print(int ha, const char* an) const
     {
@@ -79,28 +89,59 @@ class Option : public option
         }
         fprintf(stdout, "\n\n");
         
-        fprintf(stdout, "    %-20s %-13s %s\n", "long option", "arg", "description");
+        fprintf(stdout, "    %-20s %-13s %s\n", "long option", "arg", "Description");
         for (int i = 0; i < cnt-1; i++)
         {
             options[i].PrintUsage(descr[i], argname[i]);
         }
 
-        fprintf(stdout, 
-        "\nfixpath processes the output of a make process. When nested projects are used\n"
-        "the resolving of the file names in some IDE's fails. The reported error/warning\n"
-        "location is relative to the build directory which varies. This is resolved by\n"
-        "processing the Entering and Leaving directory statements (use make -w). With\n"
-        "this information the paths are made absolute. Normally fixpath is used in the\n"
-        "folowing manner: 'make -j4 -w 2>&1 | fixpath' It processes the stdin and puts\n"
-        "the altered output to stdout. There are options to perform some filtering at the\n"
-        "same time, these options are described above. Lines starting with: \n"
-        "'clang', 'gcc', 'clang++', 'g++', 'c++', 'CC' and 'GCC' are classified as\n"
-        "compiler lines.");
+        string tmp =
+            "Fixpath processes the output of a make process. When nested projects are used "
+            "the resolving of the file names in some IDE's fails. The reported error/warning "
+            "location is relative to the build directory which varies. This is resolved by "
+            "processing the Entering and Leaving directory statements (use make -w). With "
+            "this information the paths are made absolute. Normally fixpath is used in the "
+            "following manner: `make -j4 -w 2>&1 | fixpath` It processes the stdin and puts "
+            "the altered output to stdout. There are options to perform some filtering at the "
+            "same time, these options are described above. Lines starting with: "
+            "'clang', 'gcc', 'clang++', 'g++', 'c++', 'CC' and 'GCC' are classified as "
+            "compiler lines.";
+
+        string descr;
+        while (tmp.length() > 80)
+        {
+            int pos = tmp.rfind(' ', 80);
+            if (pos == string::npos)
+                break;
+            descr += tmp.substr(0, pos);
+            descr += '\n';
+            tmp.erase(0, pos+1);
+        }
+        descr += tmp;
+
+        cout << descr << endl;
+    }
+    static string GetOpt(const Option* options)
+    {
+        string opt;
+        for (int i = 0; i < cnt-1; i++)
+        {
+            if (64 < options[i].val && options[i].val < 123)
+            {
+                opt += char(options[i].val);
+                if (options[i].has_arg)
+                {
+                    opt += ':';
+                }
+            }
+        }
+
+        return opt;
     }
 };
 
 int Option::cnt = 0;
-const char* Option::descr[64];
+string Option::descr[64];
 const char* Option::argname[64];
 
 int main(int argc, char** argv)
@@ -112,15 +153,24 @@ int main(int argc, char** argv)
     bool appendReadyLine = 0;
     
     static Option options[] = { 
-        Option("help",            no_argument,        0,  'h', "Print this help"), 
-        Option("version",         no_argument,        0,  'v', "Shows the current version of fixpath"),
-        Option("compress",        no_argument,        0,  'x', "Compresses recognized compiler line by\n    removing the options from the line."),
-        Option("skip_other",      no_argument,        0,  'o', "Skip lines that are not recognized as\n    compiler or Entering/Leaving directory lines."),
-        Option("skip_compiler",   no_argument,        0,  'c', "Skip lines that are recognized as\n    compiler lines."),
-        Option("skip_directory",  no_argument,        0,  'd', "Skip lines that are recognized as\n    Entering/Leaving directory lines."),
-        Option("skip_all",        no_argument,        0,  'a', "Skip all lines from output."),
-        Option("skip_most",       no_argument,        0,  'm', "Skip all lines from output except those\n    recognized as compiler lines."),
-        Option("append_ready",    no_argument,        0,  'r', "Append a line at ens, so its clear we are\n    ready,"),
+        Option("help",            no_argument,        0,  'h', 
+            "Print this help."), 
+        Option("version",         no_argument,        0,  'v', 
+            "Shows the current version of fixpath."),
+        Option("remove_common",   no_argument,        0,  'x', 
+            "Removes common parts from recognized compiler line by removing the options from the line."),
+        Option("skip_other",      no_argument,        0,  'o', 
+            "Skip lines that are not recognized as compiler or Entering/Leaving directory lines."),
+        Option("skip_compiler",   no_argument,        0,  'c', 
+            "Skip lines that are recognized as compiler lines."),
+        Option("skip_directory",  no_argument,        0,  'd', 
+            "Skip lines that are recognized as Entering/Leaving directory lines."),
+        Option("skip_all",        no_argument,        0,  'a', 
+            "Skip all lines from output."),
+        Option("skip_most",       no_argument,        0,  'm', 
+            "Skip all lines from output except those recognized as compiler lines."),
+        Option("append_ready",    no_argument,        0,  'r', 
+            "Append a line at end, so its clear we are ready."),
 //        Option("input",           required_argument,  0,  'f', "Use infile instead of stdin.", "infile"),
 //        Option("output",          required_argument,  0,  'o', "Use outfile instead of stdout.", "infile"),
         Option(0,                 0,                  0,  0)
@@ -128,7 +178,7 @@ int main(int argc, char** argv)
 
     int long_index =0;
     int opt;
-    while ((opt = getopt_long(argc, argv,"hvxocdamr", 
+    while ((opt = getopt_long(argc, argv, Option::GetOpt(options).c_str(), 
                    options, &long_index )) != -1) {
         switch (opt) 
         {
